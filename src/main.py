a74204a367deff12849a3025723558783358547e
Samir Benzada <samirbenzada@gmail.com>
2020-02-29 21:08:30 +0000

    update

--- src/main.py
+++ src/main.py
@@ -7,8 +7,8 @@ from flask_migrate import Migrate
 from flask_swagger import swagger
 from flask_cors import CORS
 from utils import APIException, generate_sitemap
-from models import db
-#from models import Person
+from models import db, Users, datarecord
+from flask_jwt_simple import JWTManager, jwt_required, create_jwt
 
 app = Flask(__name__)
 app.url_map.strict_slashes = False
@@ -18,24 +18,132 @@ MIGRATE = Migrate(app, db)
 db.init_app(app)
 CORS(app)
 
+app.config['JWT_SECRET_KEY'] = 'dfsh3289349yhoelqwru9g'
+jwt = JWTManager(app)
+
 # Handle/serialize errors like a JSON object
 @app.errorhandler(APIException)
 def handle_invalid_usage(error):
     return jsonify(error.to_dict()), error.status_code
 
-# generate sitemap with all your endpoints
 @app.route('/')
-def sitemap():
-    return generate_sitemap(app)
+def home():
+    return "<div style='text-align: center; background-color: orange'><h1>Backend running...</h1><br/><h3>Welcome back samir</h3><img src='https://media.gettyimages.com/photos/woman-sitting-by-washing-machine-picture-id117852649?s=2048x2048' width='80%' /></div>"
 
-@app.route('/hello', methods=['POST', 'GET'])
-def handle_hello():
+@app.route('/users', methods=['GET'])
+def handle_users():
 
-    response_body = {
-        "hello": "world"
-    }
+    if request.method == 'GET':
+        users = Users.query.all()
 
-    return jsonify(response_body), 200
+        if not users:
+            return jsonify({'msg':'User not found'}), 404
+
+        return jsonify( [x.serialize() for x in users] ), 200
+
+    return "Invalid Method", 404
+
+
+@app.route('/login', methods=['POST'])
+def handle_login():
+
+    body = request.get_json()
+
+    user = Users.query.filter_by(email=body['email'], password=sha256(body['password'])).first()
+
+    if not user:
+        return 'User not found', 404
+
+    return jsonify({
+              'token': create_jwt(identity=1),
+              'id': user.id,
+              'email': user.email,
+              'firstname': user.firstname,
+              'lastname': user.lastname,
+              'avatar': user.avatar,
+              'wallet': user.wallet
+              })
+
+@app.route('/register', methods=['POST'])
+def handle_register():
+
+    body = request.get_json()
+
+    if body is None:
+        raise APIException("You need to specify the request body as a json object", status_code=400)
+    if 'firstname' not in body and 'lastname' not in body:
+        raise APIException("You need to specify the first name and last name", status_code=400)
+    if 'password' not in body and 'email' not in body:
+        raise APIException("You need to specify the password and email", status_code=400)
+    if 'firstname' not in body:
+        raise APIException('You need to specify the first name', status_code=400)
+    if 'lastname' not in body:
+        raise APIException('You need to specify the last name', status_code=400)
+    if 'password' not in body:
+        raise APIException('You need to specify the password', status_code=400)
+    if 'email' not in body:
+        raise APIException('You need to specify the email', status_code=400)
+
+
+
+    db.session.add(Users(
+        email = body['email'],
+        firstname = body['firstname'],
+        lastname = body['lastname'],
+        password = sha256(body['password'])
+    ))
+    db.session.commit()
+
+    return jsonify({
+        'register': 'success',
+        'msg': 'Successfully Registered'
+    })
+
+@app.route('/traindata', methods=['POST'])
+def get_traindata():
+    if request.method == 'POST':
+
+        body = request.get_json()
+        records = datarecord.query.filter_by(employerId=body['employerId']).order_by(datarecord.dateAtten.desc())
+
+        if not records:
+            return jsonify({'msg':'Record not found'}), 404
+
+        return jsonify( [x.serialize() for x in records] ), 200
+
+    return "Invalid Method", 404 
+
+@app.route('/addrecord', methods=['POST'])
+def record_add():
+    body = request.get_json()
+
+    if request.method == 'POST':
+        db.session.add(datarecord(
+            employerId = body['employerId'],
+            hasRecu = body['hasRecu'],
+            descriptionName = body['descriptionName'],
+            dateAtten = body['dateAtten'],
+            ceCo = body['ceCo'],
+            trainingGroup = body['trainingGroup'],
+            name = body['name'],
+            hours = body['hours'],
+            days = body['days'],
+            sta = body['sta'],
+            anp = body['anp'],
+            insIni = body['insIni'],
+            recurrent = body['recurrent'],
+            oneYearExpire = body['oneYearExpire'],
+            twoYearExpire = body['twoYearExpire'],
+            threeYearExpire = body['threeYearExpire'],
+            fourYearExpire = body['fourYearExpire']
+        ))
+
+
+        db.session.commit()
+        return jsonify({
+            'added': 'success',
+            'msg': 'Successfully Added'
+        })
 
 # this only runs if `$ python src/main.py` is executed
 if __name__ == '__main__':
--- src/models.py
+++ src/models.py
@@ -2,16 +2,67 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
-# class Person(db.Model):
-#     id = db.Column(db.Integer, primary_key=True)
-#     username = db.Column(db.String(80), unique=True, nullable=False)
-#     email = db.Column(db.String(120), unique=True, nullable=False)
+class Users(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    firstname = db.Column(db.String(120), nullable=False)
+    lastname = db.Column(db.String(120) )
+    password = db.Column(db.String(80), nullable=False)
+    avatar = db.Column(db.String(220), default='avatar.png')
+    
 
-#     def __repr__(self):
-#         return '<Person %r>' % self.username
+    def __repr__(self):
+        return '<Users %r>' % self.email
 
-#     def serialize(self):
-#         return {
-#             "username": self.username,
-#             "email": self.email
-#         }
+    def serialize(self):
+        return {
+            "id": self.id,
+            "email": self.email,
+            "firstname": self.firstname,
+            "lastname": self.lastname,
+            "avatar": self.avatar,
+        }
+
+class datarecord(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    employerId = db.Column(db.Integer)
+    courseNumber = db.Column(db.Integer)
+    hasRecu = db.Column(db.String(10)) 
+    descriptionName = db.Column(db.String(60))
+    dateAtten = db.Column(db.String(30))
+    ceCo = db.Column(db.String(12))
+    trainingGroup = db.Column(db.String(30))
+    name = db.Column(db.String(40))
+    hours = db.Column(db.String(3))
+    days = db.Column(db.Float(7, 2))
+    sta = db.Column(db.String(1))
+    anp = db.Column(db.Integer)
+    insIni = db.Column(db.String(3))
+    recurrent = db.Column(db.String(1)) 
+    oneYearExpire =  db.Column(db.String(30))
+    twoYearExpire =  db.Column(db.String(30))
+    threeYearExpire =  db.Column(db.String(30))
+    fourYearExpire =  db.Column(db.String(30))
+
+    def serialize(self):
+        return {
+            "id" : self.id,
+            "employerId": self.employerId,
+            "courseNumber" : self.courseNumber,
+            "hasRecu" : self.hasRecu,
+            "descriptionName" : self.descriptionName,
+            "dateAtten" : self.dateAtten,
+            "ceCo" : self.ceCo,
+            "trainingGroup" : self.trainingGroup,
+            "name" : self.name,
+            "hours" : self.hours,
+            "days" : self.days,
+            "sta" : self.sta,
+            "anp" : self.anp,
+            "insIni" : self.insIni,
+            "recurrent" : self.recurrent,
+            "oneYearExpire" : self.oneYearExpire,
+            "twoYearExpire" : self.twoYearExpire,
+            "threeYearExpire" : self.threeYearExpire,
+            "fourYearExpire" : self.fourYearExpire
+        }
